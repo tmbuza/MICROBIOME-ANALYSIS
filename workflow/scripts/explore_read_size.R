56d615f4e46d48a6f4309480c88e90e2a3badcd9
tmbuza <ndelly@gmail.com>
2023-03-01 00:37:03 -0600

    Finalize draft doc

--- workflow/scripts/explore_read_size.R
+++ workflow/scripts/explore_read_size.R
@@ -2,22 +2,40 @@
 
 library(tidyverse, suppressPackageStartupMessages())
 
-metadata <- read_csv("data/metadata/bushmeat.csv", show_col_types = FALSE) 
-
-
-metadata %>% 
-  dplyr::select(isolate, ecosystem, milionbases) %>% 
+# Ascending order
+ read_csv("data/metadata/tidy_PRJNA477349.csv", show_col_types = FALSE) %>%
+  dplyr::select(isolate, ecosystem, mb_bases) %>% 
   group_by(isolate, ecosystem) %>% 
-  summarise(milionbases = sum(milionbases), .groups = "drop") %>% 
-  arrange(milionbases) %>% 
-  head() %>% 
-  write_csv("results/read_size_asce.csv")
+  summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
+  arrange(mb_bases) %>% 
+ head() %>% 
+  write_csv("results/PRJNA477349_read_size_asce.csv")
 
-
-metadata %>% 
-  dplyr::select(isolate, ecosystem, milionbases) %>% 
+# Descending order
+ read_csv("data/metadata/tidy_PRJNA477349.csv", show_col_types = FALSE) %>%
+  dplyr::select(isolate, ecosystem, mb_bases) %>% 
   group_by(isolate, ecosystem) %>% 
-  summarise(milionbases = sum(milionbases), .groups = "drop") %>% 
-  arrange(-milionbases) %>% 
+  summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
+  arrange(-mb_bases) %>% 
+ head() %>% 
+  write_csv("results/PRJNA477349_read_size_desc.csv")
+
+
+
+# Ascending order
+ read_csv("data/metadata/tidy_PRJNA685168.csv", show_col_types = FALSE) %>% 
+  dplyr::select(sample_id, antibiotics, biologic, mb_bases) %>% 
+  group_by(sample_id, antibiotics, biologic) %>% 
+  summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
+  arrange(mb_bases) %>% 
   head() %>% 
-  write_csv("results/read_size_desc.csv")
+  write_csv("results/PRJNA685168_read_size_asce.csv")
+
+# Descending order
+ read_csv("data/metadata/tidy_PRJNA685168.csv", show_col_types = FALSE) %>% 
+  dplyr::select(sample_id, antibiotics, biologic, mb_bases) %>% 
+  group_by(sample_id, antibiotics, biologic) %>% 
+  summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
+  arrange(-mb_bases) %>% 
+  head() %>% 
+  write_csv("results/PRJNA685168_read_size_desc.csv")
--- workflow/scripts/get_sample_gps.R
+++ workflow/scripts/get_sample_gps.R
@@ -1,9 +1,23 @@
 #!/Users/tmbuza/opt/anaconda3/envs/snakemake/bin/Rscript
 
-source("workflow/scripts/common.R")
 library(tidyverse, suppressPackageStartupMessages())
 
-metadata <- read_csv("data/metadata/bushmeat.csv", show_col_types = FALSE)
+metadata <- read_csv("data/metadata/PRJNA477349.csv", show_col_types = FALSE) %>%  
+  rename_all(tolower) %>% 
+  rename(sample_id = run) %>% 
+  drop_na(lat_lon) %>% 
+  mutate(
+    geo_loc_name = str_replace_all(geo_loc_name, "Tanzania: ", ""),
+    geo_loc_name = str_replace_all(geo_loc_name, "The Greater Serengeti Ecosystem", "Serengeti"),
+    geo_loc_name = str_replace_all(geo_loc_name, " Ecosystem", ""),
+    isolate = str_replace_all(isolate, "_\\d*$", ""),
+    lat_lon = str_replace_all(lat_lon, " E$", ""),
+    latitude = as.numeric(str_replace_all(lat_lon, " S.*", "")) * -1,
+    longitude = as.numeric(str_replace_all(lat_lon, ".*S ", ""))) %>% 
+  rename(ecosystem = geo_loc_name) %>%
+  rename(description = host) %>% 
+  mutate(bases = round(bases/1E6, digits = 0)) %>% 
+  select(sample_id, ecosystem, isolate, latitude, longitude, mb_bases=bases, description)
 
 # Sample GPS
 library(leaflet)
@@ -25,6 +39,46 @@ m <- metadata %>%
   addCircles(color="magenta", radius = log1p(metadata$longitude) * 10)
 
 ## save html to png
-saveWidget(m, "images/sample_gps.html", selfcontained = FALSE)
-webshot("images/sample_gps.html", file = "images/sample_gps.png",
+saveWidget(m, "images/PRJNA477349_gps.html", selfcontained = FALSE)
+webshot("images/PRJNA477349_gps.html", file = "images/PRJNA477349_gps.png",
+        cliprect = "viewport")
+
+
+
+# NCBI BioProject PRJNA802976: IBD responses to biologic therapies study
+library(tidyverse, suppressPackageStartupMessages())
+
+metadata <- read_csv("data/metadata/PRJNA802976.csv", show_col_types = FALSE) %>%  
+  rename_all(tolower) %>%
+  rename(sample_id = run) %>% 
+  distinct(., sample_id, .keep_all = TRUE) %>% 
+  mutate(
+    lat_lon = str_replace_all(lat_lon, " E$", ""),
+    latitude = as.numeric(str_replace_all(lat_lon, " N.*", "")),
+    longitude = as.numeric(str_replace_all(lat_lon, ".*N ", ""))) %>% 
+  mutate(bases = round(bases/1E6, digits = 0)) %>% 
+  select(sample_id, latitude, longitude, mb_bases=bases)
+
+# Sample GPS
+library(leaflet)
+library(leaflet.providers)
+library(htmlwidgets)
+library(webshot)
+library(mapview)
+
+minLat <- min(metadata$latitude) - 1
+minLon <- min(metadata$longitude) + 0
+maxLat <- max(metadata$latitude) + .5
+maxLon <- max(metadata$longitude) + 0
+
+m <- metadata %>%
+  leaflet() %>% 
+  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+  fitBounds(minLon, minLat, maxLon, maxLat) %>%
+  addMarkers(lng = ~longitude, lat = ~latitude, popup = ~sample_id, label = ~ c(sample_id)) %>%
+  addCircles(color="magenta", radius = log1p(metadata$longitude) * 10)
+
+## save html to png
+saveWidget(m, "images/PRJNA802976_gps.html", selfcontained = FALSE)
+webshot("images/PRJNA802976_gps.html", file = "images/PRJNA802976_gps.png",
         cliprect = "viewport")
