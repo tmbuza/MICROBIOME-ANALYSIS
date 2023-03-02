e474fe52c1330a04a20c15e1aad8749bd76f5ccb
tmbuza <ndelly@gmail.com>
2023-03-02 12:50:26 -0600

    Complete draft of sample metadata repo

--- workflow/scripts/explore_read_size.R
+++ workflow/scripts/explore_read_size.R
@@ -3,16 +3,16 @@
 library(tidyverse, suppressPackageStartupMessages())
 
 # Ascending order
- read_csv("data/metadata/tidy_PRJNA477349.csv", show_col_types = FALSE) %>%
+ read_csv("data/metadata/PRJNA477349_tidy_metadata.csv", show_col_types = FALSE) %>%
   dplyr::select(isolate, ecosystem, mb_bases) %>% 
   group_by(isolate, ecosystem) %>% 
   summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
   arrange(mb_bases) %>% 
  head() %>% 
-  write_csv("results/PRJNA477349_read_size_asce.csv")
+  write_csv("results/PRJNA477349_read_size_asc.csv")
 
 # Descending order
- read_csv("data/metadata/tidy_PRJNA477349.csv", show_col_types = FALSE) %>%
+ read_csv("data/metadata/PRJNA477349_tidy_metadata.csv", show_col_types = FALSE) %>%
   dplyr::select(isolate, ecosystem, mb_bases) %>% 
   group_by(isolate, ecosystem) %>% 
   summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
@@ -23,16 +23,16 @@ library(tidyverse, suppressPackageStartupMessages())
 
 
 # Ascending order
- read_csv("data/metadata/tidy_PRJNA685168.csv", show_col_types = FALSE) %>% 
+ read_csv("data/metadata/PRJNA685168_tidy_metadata.csv", show_col_types = FALSE) %>% 
   dplyr::select(sample_id, antibiotics, biologic, mb_bases) %>% 
   group_by(sample_id, antibiotics, biologic) %>% 
   summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
   arrange(mb_bases) %>% 
   head() %>% 
-  write_csv("results/PRJNA685168_read_size_asce.csv")
+  write_csv("results/PRJNA685168_read_size_asc.csv")
 
 # Descending order
- read_csv("data/metadata/tidy_PRJNA685168.csv", show_col_types = FALSE) %>% 
+ read_csv("data/metadata/PRJNA685168_tidy_metadata.csv", show_col_types = FALSE) %>% 
   dplyr::select(sample_id, antibiotics, biologic, mb_bases) %>% 
   group_by(sample_id, antibiotics, biologic) %>% 
   summarise(mb_bases = sum(mb_bases), .groups = "drop") %>% 
--- workflow/scripts/get_run_accessions.py
+++ workflow/scripts/get_run_accessions.py
@@ -3,29 +3,29 @@ import sys
 import csv
 import pandas as pd
 
-METADATA=pd.read_csv('data/metadata/runinfo_PRJNA477349.csv').loc[0:9]
-ACCESSIONS=METADATA['Run'].tolist()
+METADATA=pd.read_csv('data/metadata/PRJNA477349_tidy_metadata.csv').loc[0:9]
+ACCESSIONS=METADATA['sample_id'].tolist()
 
-with open('data/metadata/PRJNA477349_sra_accessions.txt', 'w') as f:
+with open('results/PRJNA477349_sra_accessions.txt', 'w') as f:
     print(ACCESSIONS, file=f)
 
 
-METADATA=pd.read_csv('data/metadata/runinfo_PRJNA802976.csv').loc[0:9]
-ACCESSIONS=METADATA['Run'].tolist()
+METADATA=pd.read_csv('data/metadata/PRJNA802976_tidy_metadata.csv').loc[0:9]
+ACCESSIONS=METADATA['sample_id'].tolist()
 
-with open('data/metadata/PRJNA802976_sra_accessions.txt', 'w') as f:
+with open('results/PRJNA802976_sra_accessions.txt', 'w') as f:
     print(ACCESSIONS, file=f)
 
 
-METADATA=pd.read_csv('data/metadata/runinfo_PRJNA685168.csv').loc[0:9]
-ACCESSIONS=METADATA['Run'].tolist()
+METADATA=pd.read_csv('data/metadata/PRJNA685168_tidy_metadata.csv').loc[0:9]
+ACCESSIONS=METADATA['sample_id'].tolist()
 
-with open('data/metadata/PRJNA685168_sra_accessions.txt', 'w') as f:
+with open('results/PRJNA685168_sra_accessions.txt', 'w') as f:
     print(ACCESSIONS, file=f)
 
 
-METADATA=pd.read_csv('data/metadata/runinfo_PRJEB21612.csv').loc[0:9]
-ACCESSIONS=METADATA['Run'].tolist()
+METADATA=pd.read_csv('data/metadata/PRJEB21612_tidy_metadata.csv').loc[0:9]
+ACCESSIONS=METADATA['sample_id'].tolist()
 
-with open('data/metadata/PRJEB21612_sra_accessions.txt', 'w') as f:
+with open('results/PRJEB21612_sra_accessions.txt', 'w') as f:
     print(ACCESSIONS, file=f)
